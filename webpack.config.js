6bf8f25c01c7ce0a24a47d27873c75ceb4ae119e
Ali Torki <A.Torki@alibaba.ir>
2017-06-24 21:47:35 +0430

    remove: react-error-overlay

--- webpack.config.js
+++ webpack.config.js
@@ -22,22 +22,9 @@ const config = {
 	// Don't attempt to continue if there are any errors.
 	bail: true,
 	entry: [
-		// Include an alternative client for WebpackDevServer. A client's job is to
-		// connect to WebpackDevServer by a socket and get notified about changes.
-		// When you save a file, the client will either apply hot updates (in case
-		// of CSS changes), or refresh the page (in case of JS changes). When you
-		// make a syntax error, this client will display a syntax error overlay.
-		// Note: instead of the default WebpackDevServer client, we use a custom one
-		// to bring better experience for Create React App users. You can replace
-		// the line below with these two lines if you prefer the stock client:
-		// require.resolve('webpack-dev-server/client') + '?/',
-		// require.resolve('webpack/hot/dev-server'),
-		require.resolve('react-dev-utils/webpackHotDevClient'),
-		// We ship a few polyfills by default:
+		'webpack/hot/only-dev-server',
+		'webpack-dev-server/client?http://'+ HOST +':'+ PORT,
 		require.resolve('./webpack/polyfills'),
-		// Errors should be considered fatal in development
-		require.resolve('react-error-overlay'),
-
 		// Finally, this is your app's code:
 		paths.appIndexJs,
 	],
@@ -80,7 +67,7 @@ const config = {
 		host: HOST,
 		hot: true,
 		quiet: false,
-		inline: true,
+		overlay:true,
 		stats: {
 			colors: true
 		},
