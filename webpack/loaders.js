a7fb4ee3c223520ffd2e29c508fa4096dcfaf50c
Ali Torki <A.Torki@alibaba.ir>
2017-06-24 20:26:58 +0430

    add: file and url loader

--- webpack/loaders.js
+++ webpack/loaders.js
@@ -1,6 +1,42 @@
 const findCacheDir = require('find-cache-dir');
 
 const loaders = [
+	// ** ADDING/UPDATING LOADERS **
+	// The "file" loader handles all assets unless explicitly excluded.
+	// The `exclude` list *must* be updated with every change to loader extensions.
+	// When adding a new loader, you must add its `test`
+	// as a new entry in the `exclude` list for "file" loader.
+
+	// "file" loader makes sure those assets get served by WebpackDevServer.
+	// When you `import` an asset, you get its (virtual) filename.
+	// In production, they would get copied to the `build` folder.
+	{
+		exclude: [
+			/\.html$/,
+			/\.(js|jsx)$/,
+			/\.css$/,
+			/\.json$/,
+			/\.bmp$/,
+			/\.gif$/,
+			/\.jpe?g$/,
+			/\.png$/,
+		],
+		loader: "file-loader",
+		options: {
+			name: "static/media/[name].[hash:7].[ext]",
+		},
+	},
+	// "url" loader works like "file" loader except that it embeds assets
+	// smaller than specified limit in bytes as data URLs to avoid requests.
+	// A missing `test` is equivalent to a match.
+	{
+		test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+		loader: "url-loader",
+		options: {
+			limit: 10000,
+			name: 'static/media/[name].[hash:7].[ext]',
+		},
+	},
 	{
 		test: /\.jsx?$/,
 		exclude: /node_modules/,
